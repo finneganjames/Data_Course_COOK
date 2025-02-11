72a39ce230cba5535a08cb80f60504d08099bc1a
finneganjames <finn.cook8@gmail.com>
2025-02-10 20:28:39 -0700

    Relative filepath Exam 1

--- Exams/Exam_1/Exam_1_10881837.R
+++ Exams/Exam_1/Exam_1_10881837.R
@@ -2,22 +2,22 @@
 
 library(tidyverse)
 
-df <- read_csv("../../cleaned_covid_data.csv")
+df <- read_csv("Exams/Exam_1/cleaned_covid_data.csv")
 
 A_states <- subset(df, grepl('a', ignore.case = TRUE, Province_State))
 
 ggplot(data = A_states, mapping = aes(x = Last_Update,
                                y = Deaths)) + 
   geom_point()+ 
-  facet_wrap(~Province_State) + 
-  geom_smooth(se = FALSE)
+  facet_wrap(~Province_State, scales = "free") + 
+  geom_smooth(method = "loess", se = FALSE)
+
 
 state_max_fatality_rate <- df %>%
    select(Case_Fatality_Ratio, Province_State)
- 
- # need to triple check this one 
- 
-order(state_max_fatality_rate$Case_Fatality_Ratio, na.last = TRUE, decreasing = TRUE)
+
+
+order(state_max_fatality_rate$Case_Fatality_Ratio, decreasing = TRUE)
 
 
 ggplot(data = state_max_fatality_rate, mapping = aes(x = reorder(Province_State, Case_Fatality_Ratio, desc = TRUE), 
