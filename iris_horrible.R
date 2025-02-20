6d8faf36f7705bcb702b699697680fc7eb44a108
finneganjames <finn.cook8@gmail.com>
2025-02-20 12:45:15 -0700

    updated colors for iris_horrible

--- iris_horrible.R
+++ iris_horrible.R
@@ -1,21 +1,18 @@
 library(tidyverse)
 library(ggimage)
 library(ggplot2)
-
+library(jpeg)
 
 df <- iris 
 img = "https://i.pinimg.com/736x/77/aa/21/77aa2192ee76059b4c8abf3291ee76d2.jpg"
 
-cross_stitch <- ggplot(data = df) +
-  aes(x=Petal.Width) +
-  aes(y=Petal.Length) +
+cross_stitch <- ggplot(df, aes(x=Petal.Width, y=Petal.Length, color = Species)) +
   geom_point(size = 3, shape = 4, stroke = 2) +
-  aes(color=Species) + 
-  scale_color_manual(values = c())
   theme(
     panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank()
-  )  
+    panel.grid.minor = element_blank()) + scale_color_manual(values = c("setosa" = "darkorange",
+                                    "veriscolor" = "darkorange", 
+                                    "virginica" = "darkred"))
 
 ggbackground(cross_stitch, img)
 
